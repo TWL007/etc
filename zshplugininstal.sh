7f08e2bb09bdd9929408f54ebd6cad248b380ff1
Tian Cao <caotian@momenta.ai>
2018-09-23 23:53:14 +0800

    new

--- zshplugininstal.sh
+++ zshplugininstal.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+cd ~
+ln -sf .etc/zshrc .zshrc
+ln -sf .etc/vimrc .vimrc
+ln -sf .etc/tmux.conf .tmux.conf
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins
