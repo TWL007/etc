24dd05491fa189b573a002536a34dc20d94431a6
wh0 <wh0@0x0neo.momenta.ai>
2018-10-31 04:57:01 -0400

    change tmux

--- other.sh
+++ other.sh
@@ -4,3 +4,5 @@ cd ~
 ln -sf .etc/vimrc .vimrc
 ln -sf .etc/tmux.conf .tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+bash ~/.tmux/plugins/tpm/bin/install_plugins
