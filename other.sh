dbb13cfda7a11b21bfbf1cfc10fd25eccf8ec438
Tian Cao <caotian@momenta.ai>
2018-11-06 17:56:33 +0800

    fix ls

--- other.sh
+++ other.sh
@@ -3,6 +3,10 @@
 cd ~
 ln -sf .etc/vimrc .vimrc
 ln -sf .etc/tmux.conf .tmux.conf
+ln -sf .etc/zshrc .zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 bash ~/.tmux/plugins/tpm/bin/install_plugins
+cd ~/.etc
+git submodule init
+git submodule update
