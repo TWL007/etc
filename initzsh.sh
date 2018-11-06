2feb06946318c14a4253504929e3e955998fbce2
Tian Cao <caotian@momenta.ai>
2018-11-06 18:03:58 +0800

    fix ls

--- initzsh.sh
+++ initzsh.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd ~
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+yum install -y zsh git vim screen ctags
--- other.sh
+++ other.sh
@@ -7,6 +7,3 @@ ln -sf .etc/zshrc .zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 bash ~/.tmux/plugins/tpm/bin/install_plugins
-cd ~/.etc
-git submodule init
-git submodule update
